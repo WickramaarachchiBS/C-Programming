207eb603be7e17818ae124c431a00eb7bfb8493c
WickramaarachchiBS <132426591+WickramaarachchiBS@users.noreply.github.com>
2023-06-05 16:07:18 +0530

    lab 3.0

---
+++ 3.0.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,r;
+    printf("Enter two numbers ");
+    scanf("%d %d",&n1,&n2);
+
+    if(n1%n2==0)
+
+           printf("First number is a multiplication of the second number ");
+
+
+    else
+
+           printf("First number is not a multiplication of the second number ");
+
+}
